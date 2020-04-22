c793e833891cfd99a2da9d469db61ac2f6206f9d
Shlok <shlokmohanty2000@gmail.com>
2020-04-22 15:32:49 +0530

    open and closed a file using code

---
+++ filein.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+int main()
+{
+	string name;
+	int roll;
+	string branch;
+	ifs>>name>>roll>>branch;
+	ifs.close();
+	cout<<"name"<<name<<endl;
+	cout<<"roll"<<roll<<endl;
+	cout<<"branch"<<branch<<endl;
+	ifs.close()
+}
+
